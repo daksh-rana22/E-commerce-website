88fb74b5b078aae4d5fb06ed08f5626137e5c812
daksh-rana22 <ranadaksh2004@gmail.com>
2025-08-18 00:37:33 +0530

    Initial commit with full project

---
+++ frontend/src/Components/Product.js
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrum from "./Breadcrum/Breadcrum";
+import ProductDisplay from "./components/ProductDisplay/ProductDisplay";
+import Description from "./DescriptionBox/Description";
+import RelatedProducts from "./RelatedProducts/RelatedProduct";
+
+
+
+const Product=()=>{
+
+
+const {all_product}=useContext(ShopContext);
+
+const {productId}=useParams();
+
+const product=all_product.find((e)=> e.id ===Number(productId));
+
+return(
+    <div>
+    <Breadcrum product={product}/>
+
+    <ProductDisplay product={product}/>
+
+    <Description/>
+
+    <RelatedProducts/>
+
+    </div>
+
+
+);
+}
+export default Product;
